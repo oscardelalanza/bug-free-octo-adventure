eddea37d3ea06c2544c62f5333c657d2db71fb2c
oscardelalanza <oscardelalanza@gmail.com>
2021-10-25 00:46:39 -0700

    destructuring styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,27 @@ interface Props {
   state: string
 }
 
+const style = ({bg = '#222'}) => ({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px'
+})
+
+const style2 = {
+  boxShadow: '0 5px 3px rgba(0,0,0,0.1)'
+}
+
 const Li = ({children}: Props) => {
   return (
-    <li className='li'>{children}</li>
+    <li style={{...style2, ...style({bg: '#333'})}} className='li'>{children}</li>
   )
 }
 
 const App = () => {
   const value = 'De La Lanza'
   return (
-    <ul className="css-class">
+    <ul style={style({bg: '#750'})}className="css-class">
       <Li state='happy'>Valor de Li</Li>
     </ul>
   )
