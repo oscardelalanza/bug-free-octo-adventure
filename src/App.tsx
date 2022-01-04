7e60092fb22804eb4525e4bd24ccde1d45c0a8f9
oscardelalanza <oscardelalanza@gmail.com>
2022-01-04 14:04:30 -0800

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import Button from './Button'
 
 const App = () => {
+  const myVar: boolean = true;
+
+  if (myVar) {
+    return <p>Mi variable es true</p>
+  }
+
   return (
     <div>
       <h1 onClick={(e) => console.log('click', e)}>Hola mundo</h1>
